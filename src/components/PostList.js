3c179faa8b085b8b959a70edea93ac84d355e4b6
Imani Oliver <isaac@ihardy.co>
2017-09-25 00:56:35 -0400

    blog entries appear on the page!

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -2,9 +2,53 @@ import React, { Component } from 'react';
 
 export default class PostList extends Component{
 
+        constructor(){
+            super();
+
+            this.state= {
+                "authorName":"",
+                "blogTitle":"",
+                "blogEntry":"",
+                entries:[]
+
+
+            }
+        }
+
+
+
+        componentDidMount(){
+            fetch('https://tiny-lasagna-server.herokuapp.com/collections/blogger/')
+            .then(results => {
+                return results.json();
+            })
+            // .then(stuff=>{
+            //     console.log("the data: ", stuff);
+            // })
+            .then(data => {
+                let entries = data.map((entry, index) =>{
+                    return (
+                        <div class="card">
+                          <div class="card-block">
+                            <h4 class="card-title">Title: {entry.blogTitle}</h4>
+                            <p class="card-text">By <strong>{entry.authorName}</strong></p>
+                          </div>
+                          <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Cras justo odio</li>
+                            <li class="list-group-item">Dapibus ac facilisis in</li>
+                            <li class="list-group-item">Vestibulum at eros</li>
+                          </ul>
+
+                        </div>
+                    )
+                 })
+            });
+            this.setState({entries: entries});
+        }
     render(){
         return(
             <div>
+            {this.state.entries}
 
             </div>
 
